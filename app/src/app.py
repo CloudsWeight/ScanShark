d8779c6c6675189dcd3d76b87544f0119b368e89
CloudsWeight <CloudsWeight@users.noreply.github.com>
2025-12-29 00:48:45 -0800

    Update app.py

--- app/src/app.py
+++ app/src/app.py
@@ -1,11 +1,5 @@
 '''
-Current:  Simple route '/' that returns data from a random currency pair
-
-To Do:  Create multiple currency pair inputs that use the following parameters for the fmath.Fx.get_rates(rate,tf,count) function:
-  * rate: eurusd, usdjpy, gbpusd, eurjpy, eurgbp, chfjpy, zarjpy, usdmxn, usdchf, etcetc
-  * tf: S5, M5, M15, H1, H4, H8, D, W, M
-  * count: 1-500
-
+Current:  Simple route '/' that returns data 
 
 '''
 from fastapi import FastAPI
@@ -19,9 +13,5 @@ app = FastAPI()
 @app.get("/", tags=["Root"])
 async def read_root():
  """OLD Boilerplate code for FastAPI endpoints 
-    pair = random.choice(['eurusd', 'usdjpy', 'eurgbp', 'eurusd', 'chfjpy'])
-    rates = fx.get_rates(pair, 'H1', 30)
-    #df = to_df(rates)
-    return {"rates": rates
-            }
+    return json 
 """ 
