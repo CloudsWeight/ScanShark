208e12a38b4883aa8369006bf3415ff0b2580129
CloudsWeight <CloudsWeight@users.noreply.github.com>
2025-12-29 15:41:53 -0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app = FastAPI (
 
 app.add_middleware(
 	CORSMiddleware, 
-	allow_origin=["*"],
+	allow_origins=["*"],
 	allow_credentials=True,
 	allow_methods=["*"],
 	allow_headers=["*"]
@@ -33,4 +33,4 @@ async def health():
 	return {"status":"healthy"}
 
 if __name__ == "__main__":
-	uvicorn.run(app, host="0.0.0.0", port=43451)
+	uvicorn.run(app, host="0.0.0.0", port=3000)
