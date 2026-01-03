84fc4da29ab44ceb610ac604e413c63a770f2897
CloudsWeight <CloudsWeight@users.noreply.github.com>
2026-01-03 10:01:27 -0800

    Update webhook.py

--- main/fastapi/app/routers/webhook.py
+++ main/fastapi/app/routers/webhook.py
@@ -2,47 +2,24 @@ from fastapi import APIRouter, Request, BackgroundTasks
 from typing import Dict
 import logging
 from services.orchestrator import run_full_scan
+import os
 
 router = APIRouter()
 logger = logging.getLogger(__name__)
 
 @router.post("/github")
 async def github_webhook(request: Request, background_tasks: BackgroundTasks):
-    try:
-        payload = await request.json()
-        repo_url = payload.get('repository', {}).get('clone_url')
-        repo_name = paylod.get('repository', {}).get('fullname')
-        commit_sha = payload.get('after')
+    payload = await request.json()
 
-        if not repo_url:
-            return {"error":"No repo URL in payload"}
-
-        logger.info(f"Web hook for repo : {repo_name}, commit: {commit_sha}")
-
-        background_tasks.add_task(
-            run_full_san,
-            repo_url=repo_url,
-            repo_name=repo_name,
-            commit_sha=commit_sha
-            )
-
-        return {
-            "status":"accepted",
-            "message": f"Scan queued for {repo_name}",
-            "commit":commit_sha
-        }
-
-    except Exception as e:
-        logger.error(f"Webhook error: {str(e)}")
-        return {"error": str(e)}, 500
-
+    return payload
 
 @router.get("/test")
-async def test_tebhook():
+async def test_webhook():
     test_repo = "https://github.com/CloudsWeight/ScanShark"
     result = await run_full_scan(
         repo_url=test_repo,
         repo_name="CloudsWeight/ScanShark",
         commit_sha="test"
         )
+    print(result)
     return result
