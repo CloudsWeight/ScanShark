db6bef2af4085656d78693c982539c0ab36eba86
CloudsWeight <CloudsWeight@users.noreply.github.com>
2026-01-13 12:04:36 -0800

    Update webhook.py

--- main/fastapi/app/routers/webhook.py
+++ main/fastapi/app/routers/webhook.py
@@ -2,7 +2,11 @@ from fastapi import APIRouter, Request, BackgroundTasks
 from typing import Dict
 import logging
 from services.orchestrator import run_full_scan
+from services.clone import clone
 import os
+import asyncio
+import sys
+import json
 
 router = APIRouter()
 logger = logging.getLogger(__name__)
@@ -10,16 +14,51 @@ logger = logging.getLogger(__name__)
 @router.post("/github")
 async def github_webhook(request: Request, background_tasks: BackgroundTasks):
     payload = await request.json()
+    test_repo = "https://github.com/CloudsWeight/ScanShark"
+    repo_path = clone(test_repo)
+    output_file = os.path.join(repo_path, "bandit_output.json")
+    process = await asyncio.create_subprocess_exec(
+            sys.executable,
+            "-m", "bandit",
+            "-r", repo_path,
+            "-f", "json",
+            "-o", output_file,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+            )
+    stdout, stderr = await process.communicate()  # <-- wait for bandit to finish
 
-    return payload
+    with open(output_file, "r", encoding="utf-8") as f:
+            bandit_results = json.load(f)
+
+    return {
+        "status": "success",
+        "repo_path": repo_path,
+        "bandit": bandit_results,
+        }
 
 @router.get("/test")
 async def test_webhook():
     test_repo = "https://github.com/CloudsWeight/ScanShark"
-    result = await run_full_scan(
-        repo_url=test_repo,
-        repo_name="CloudsWeight/ScanShark",
-        commit_sha="test"
-        )
-    print(result)
-    return result
+    repo_path = clone(test_repo)
+    output_file = os.path.join(repo_path, "bandit_output.json")
+    process = await asyncio.create_subprocess_exec(
+            sys.executable,
+            "-m", "bandit",
+            "-r", repo_path,
+            "-f", "json",
+            "-o", output_file,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+            )
+    stdout, stderr = await process.communicate()  # <-- wait for bandit to finish
+
+    with open(output_file, "r", encoding="utf-8") as f:
+            bandit_results = json.load(f)
+
+    return {
+        "status": "success",
+        "repo_path": repo_path,
+        "bandit": bandit_results,
+    }
+    
